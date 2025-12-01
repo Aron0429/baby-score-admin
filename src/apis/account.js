ae777e0799e059eee7b89bc38033968bc5956b57
caoqianqian <963691340@qq.com>
2025-12-01 16:18:56 +0800

    feat: 目标管理页面构建&逻辑初写

--- src/apis/account.js
+++ src/apis/account.js
@@ -1,5 +1,5 @@
 import Request from '@/utils/axiosInstance'
-
+// ---------用户管理--------
 // 查询用户列表
 export const queryUsers = async params => {
   const res = await Request.get('/admin/users', { params })
@@ -12,6 +12,7 @@ export const queryUserTypeList = async params => {
   return res.data
 }
 
+// ---------宝贝管理--------
 // 查询宝贝列表
 export const queryBabys = async params => {
   const res = await Request.get('/admin/babys', { params })
@@ -21,5 +22,18 @@ export const queryBabys = async params => {
 // 删除宝贝
 export const babyDelete = async (babyId, params) => {
   const res = await Request.delete(`/admin/baby/${babyId}`, params)
+  return res.data
+}
+
+// ---------目标管理--------
+// 查询(全部)宝贝目标列表
+export const queryTargetList = async (babyId, params) => {
+  const res = await Request.get(`/target/list/${babyId}`, params)
+  return res.data
+}
+
+// 删除目标
+export const targetDelete = async (targetId, params) => {
+  const res = await Request.delete(`/target/delete/${targetId}`, params)
   return res.data
 }
--- src/views/account/target.vue
+++ src/views/account/target.vue
@@ -1,9 +1,87 @@
-<!--
- * @Description: 
- * @Author: HuGang
- * @Date: 2025-11-30 14:40:05
- * @LastEditTime: 2025-11-30 14:40:17
--->
 <template>
-  <div>目标管理</div>
+  <div class="target-type-container">
+    <div class="search-container">
+      <el-form :inline="true" :model="queryForm" class="demo-form-inline">
+        <el-form-item label="目标名称">
+          <el-input v-model="queryForm.targetName" placeholder="请输入目标名称" clearable />
+        </el-form-item>
+
+        <el-form-item label="宝贝ID">
+          <el-input v-model="queryForm.babyId" placeholder="请输入宝贝ID" clearable />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="fetchTargets">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <el-table :data="state.tableData" stripe border style="width: 100%">
+      <el-table-column label="ID">
+        <template #default="scope">
+          <span>{{ scope.row.id }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="目标名称" prop="name"></el-table-column>
+      <el-table-column label="所属分类" prop="parentId"></el-table-column>
+      <el-table-column label="评分方式" prop="score"></el-table-column>
+      <el-table-column label="创建时间" prop="createAt"></el-table-column>
+      <el-table-column label="更新时间" prop="updateAt"></el-table-column>
+      <el-table-column label="操作" width="180">
+        <template #default="scope">
+          <el-button type="danger" @click="handleDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import utils from '@/utils'
+
+import { queryTargetList, targetDelete } from '@/apis/account.js'
+
+const queryForm = ref({
+  targetName: '',
+  babyId: null,
+})
+
+const state = ref({
+  sex: [
+    { label: '男', value: 1 },
+    { label: '女', value: 2 },
+  ],
+  tableData: [],
+})
+
+fetchTargets()
+
+async function fetchBabys() {
+  const res = await queryTargetList(queryForm.value)
+  if (res.code == 200) {
+    state.value.tableData = res.data.list.map(item => {
+      item.createAt = utils.transforDate(item.createAt)
+      item.updateAt = utils.transforDate(item.updateAt)
+      return item
+    })
+  }
+}
+
+async function handleDelete(row) {
+  const res = await targetDelete(row.id)
+  if (res.code === 200) {
+    utils.showToast('删除成功')
+    fetchTargetList()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.target-type-container {
+  width: 100%;
+
+  .search-container {
+    display: flex;
+    margin-bottom: 20px;
+  }
+}
+</style>
